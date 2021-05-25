8ef42c969c3b5a07005f5169780fac8e705574fb
logiteg00 <65267507+logiteg00@users.noreply.github.com>
2021-05-26 00:35:49 +0600

    Add files via upload

---
+++ CG TASK1/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/Entity/Effects/Animation.cpp.o"
+  "CMakeFiles/main.dir/Entity/NPC/Player/Player.cpp.o"
+  "CMakeFiles/main.dir/Entity/Textures/Symbols/Symbols.cpp.o"
+  "CMakeFiles/main.dir/Entity/Weapons/Weapon.cpp.o"
+  "CMakeFiles/main.dir/Entity/Weather/Fog.cpp.o"
+  "CMakeFiles/main.dir/Image/Image.cpp.o"
+  "CMakeFiles/main.dir/glad.c.o"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "bin/main"
+  "bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
