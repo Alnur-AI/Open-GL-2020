a97c42e920f640f899fc4766a55a0847c114e39d
logiteg00 <65267507+logiteg00@users.noreply.github.com>
2021-06-08 12:01:29 +0600

    Add files via upload

---
+++ CG TASK1/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/redragon/CG TASK1/glad.c" "/home/redragon/CG TASK1/CMakeFiles/main.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "dependencies/include/GLAD"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/redragon/CG TASK1/Entity/Effects/Animation.cpp" "/home/redragon/CG TASK1/CMakeFiles/main.dir/Entity/Effects/Animation.cpp.o"
+  "/home/redragon/CG TASK1/Entity/NPC/Player/Player.cpp" "/home/redragon/CG TASK1/CMakeFiles/main.dir/Entity/NPC/Player/Player.cpp.o"
+  "/home/redragon/CG TASK1/Entity/Textures/Symbols/Symbols.cpp" "/home/redragon/CG TASK1/CMakeFiles/main.dir/Entity/Textures/Symbols/Symbols.cpp.o"
+  "/home/redragon/CG TASK1/Entity/Weapons/Weapon.cpp" "/home/redragon/CG TASK1/CMakeFiles/main.dir/Entity/Weapons/Weapon.cpp.o"
+  "/home/redragon/CG TASK1/Entity/Weather/Fog.cpp" "/home/redragon/CG TASK1/CMakeFiles/main.dir/Entity/Weather/Fog.cpp.o"
+  "/home/redragon/CG TASK1/Image/Image.cpp" "/home/redragon/CG TASK1/CMakeFiles/main.dir/Image/Image.cpp.o"
+  "/home/redragon/CG TASK1/main.cpp" "/home/redragon/CG TASK1/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "dependencies/include/GLAD"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
